ae9bf708e9265b3fa33ddca50be956920441405b
Hirokazu Okawa <h.okyawa@gmail.com>
2022-05-18 14:55:36 +0900

    tsconfig.jsonの生成
    $ npx tsc --init

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = {
+  mode: isProduction ? "production" : "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "video-hls.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./demo/index.html"
+    })
+  ]
+};
