7ef1df01f1dc11b7100afddb36411bfd406589f1
Hirokazu Okawa <h.okyawa@gmail.com>
2022-06-15 15:47:22 +0900

    [wip] webpack.configにreact移行モード設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ const isProduction = process.env.NODE_ENV === "production";
 module.exports = {
   mode: isProduction ? "production" : "development",
   entry: {
-    'video-hls': './src/index.tsx',
-    'product-fom': './src/product-fom.tsx',
+    "video-hls": "./src/index.tsx",
+    "product-fom": "./src/product-fom.tsx",
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
@@ -28,10 +28,25 @@ module.exports = {
   },
   resolve: {
     extensions: [".ts", ".tsx"],
+    alias: {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+      // test-utilsの下にある必要があります。
+    },
   },
   plugins: [
+/*
+    // 下記のJSXの設定を加えるとエラーになるので、一旦コメントアウト
+    [
+      "@babel/plugin-transform-react-jsx", {
+        "pragma": "h",
+        "pragmaFrag": "Fragment",
+      }
+    ],
+*/
     new HtmlWebpackPlugin({
-      template: "./demo/index.html"
-    })
-  ]
+      template: "./demo/index.html",
+    }),
+  ],
 };
