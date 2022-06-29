83cf6d16800c2b5f95ffae24c8c5e66034543384
Hirokazu Okawa <h.okyawa@gmail.com>
2022-06-29 15:01:06 +0900

    watchを使ったフィールド値の監視

--- src/components/subscription-form/react-hook-form.tsx
+++ src/components/subscription-form/react-hook-form.tsx
@@ -24,12 +24,16 @@ type Props = {
 export const ReactHookForm = () => {
   const {
     register,
+    watch,
     formState: { errors },
     handleSubmit,
   } = useForm<SubscriptionFormInput>({ defaultValues: initialValues });
+
   const onSubmit: SubmitHandler<SubscriptionFormInput> = (data) => {
     console.log(data);
   };
+
+  const checkedDeliveryCycle = watch('delivery_cycle');
 
   return <>
     <h2>React Hook Formを使った検証</h2>
@@ -47,36 +51,44 @@ export const ReactHookForm = () => {
           <label><input type="radio" value="monthly" {...register('delivery_cycle', { required: true })} />週ごと</label>
           <label><input type="radio" value="weekly" {...register('delivery_cycle', { required: true })} />月ごと</label>
         </div>
-        <div>
-          <select {...register('delivery_monthly_cycle', { required: true })}>
-            {
-              Object.keys(deliveryMonthlyCycle)
-                .map((name) => <option value={name}>{deliveryMonthlyCycle[name]}</option>)
-            }
-          </select>
-          <select {...register('delivery_days', { required: true })}>
-            {
-              Object.keys(deliveryDays)
-                .map((name) => <option value={name}>{deliveryDays[name]}</option>)
-            }
-          </select>
-        </div>
-        <div>
-          <select {...register('delivery_weekly_cycle', { required: true })}>
-            {
-              Object.keys(deliveryWeeklyCycle)
-                .map((name) => <option value={name}>{deliveryWeeklyCycle[name]}</option>)
-            }
-          </select>
-          <select
-            {...register('delivery_day_of_week', { required: true })}
-          >
-            {
-              Object.keys(deliveryDayOfWeek)
-                .map((name) => <option value={name}>{deliveryDayOfWeek[name]}</option>)
-            }
-          </select>
-        </div>
+        {
+          checkedDeliveryCycle === 'monthly' && (
+            <div>
+              <select {...register('delivery_monthly_cycle', { required: true })}>
+                {
+                  Object.keys(deliveryMonthlyCycle)
+                    .map((name) => <option value={name}>{deliveryMonthlyCycle[name]}</option>)
+                }
+              </select>
+              <select {...register('delivery_days', { required: true })}>
+                {
+                  Object.keys(deliveryDays)
+                    .map((name) => <option value={name}>{deliveryDays[name]}</option>)
+                }
+              </select>
+            </div>
+          )
+        }
+        {
+          checkedDeliveryCycle === 'weekly' && (
+            <div>
+              <select {...register('delivery_weekly_cycle', { required: true })}>
+                {
+                  Object.keys(deliveryWeeklyCycle)
+                    .map((name) => <option value={name}>{deliveryWeeklyCycle[name]}</option>)
+                }
+              </select>
+              <select
+                {...register('delivery_day_of_week', { required: true })}
+              >
+                {
+                  Object.keys(deliveryDayOfWeek)
+                    .map((name) => <option value={name}>{deliveryDayOfWeek[name]}</option>)
+                }
+              </select>
+            </div>
+          )
+        }
       </fieldset>
       <div>
         <input type="submit" />
