37bddc07ff46d765ec8ec3ecd862d5ace28b7274
Hirokazu Okawa <h.okyawa@gmail.com>
2022-09-07 13:36:21 +0900

    storybook/preactから型が読み込めていない点への一時的な対応

--- src/components/hooks-form/hooks-form.stories.tsx
+++ src/components/hooks-form/hooks-form.stories.tsx
@@ -1,16 +1,17 @@
-import { Meta, StoryObj, StoryFn } from '@storybook/preact';
+// import { Meta, StoryObj, StoryFn } from '@storybook/preact';
+import * as sb from '@storybook/preact';
 import { h } from 'preact';
 
 import { HooksForm } from './hooks-form';
 
-export default { component: HooksForm } as Meta<typeof HooksForm>;
+export default { component: HooksForm } as sb.Meta<typeof HooksForm>;
 
-export const Index: StoryFn<typeof HooksForm> = () => <HooksForm />;
-export const Index2: StoryFn<typeof HooksForm> = () => <HooksForm />;
+export const Index: sb.StoryFn<typeof HooksForm> = () => <HooksForm />;
+export const Index2: sb.StoryFn<typeof HooksForm> = () => <HooksForm />;
 
 /*
 // ↓の形式だと "TypeError: Cannot read properties of undefined (reading 'length')" となる
-export const Index: StoryObj<typeof HooksForm> = {
+export const Index: sb.StoryObj<typeof HooksForm> = {
   args: { },
 };
 */
