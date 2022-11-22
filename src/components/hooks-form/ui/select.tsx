d92ec2fd8303416566fd9c9aa88210a1fd22cfba
Hirokazu Okawa <h.okyawa@gmail.com>
2022-11-22 17:25:24 +0900

    Selectコンポーネントのリファクタリング

--- src/components/hooks-form/ui/select.tsx
+++ src/components/hooks-form/ui/select.tsx
@@ -12,14 +12,14 @@ type Props = {
   dispatch: (action: ActionType) => void;
 };
 
-export const Select = ({ options, name, dataTestId, currentValue: value, dispatch }: Props) => {
+export const Select = ({ options, name, dataTestId, currentValue, dispatch }: Props) => {
   const onChange = (e: JSX.TargetedEvent<HTMLSelectElement, Event>) => {
     dispatch({ field: e.currentTarget.name, value: e.currentTarget.value });
   };
 
   return (
     <Fragment>
-      <select name={name} onChange={onChange} value={value} data-testid={dataTestId}>
+      <select name={name} onChange={onChange} value={currentValue} data-testid={dataTestId}>
         {options.map((item) => (
           <option key={item.value} value={item.value}>
             {item.label}
